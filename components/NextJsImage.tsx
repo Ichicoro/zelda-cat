b5a16afad1a4648e2cf1d4884bbf357d685968fd
Ichicoro <me@marte.dev>
2024-06-05 23:11:05 +0200

    feat: add basic full screen to gallery

--- components/NextJsImage.tsx
+++ components/NextJsImage.tsx
@@ -13,12 +13,13 @@ export default function NextJsImage({
     <div
       style={{ ...wrapperStyle, position: "relative" }}
       className={classNames(className)}
+      onClick={onClick}
     >
       <Image
         fill
         src={photo}
         placeholder={"blurDataURL" in photo ? "blur" : undefined}
-        {...{ alt, title, sizes, className: imageClassName, onClick }}
+        {...{ alt, title, sizes, className: imageClassName }}
       />
       <div className="absolute opacity-0 hover:opacity-100 w-full h-full top-0 right-0 left-0">
         <div className="absolute w-full h-[50px] bottom-0 right-0 left-0 bg-orange-100 flex justify-center items-center">
--- components/PhotoGallery.tsx
+++ components/PhotoGallery.tsx
@@ -1,30 +1,52 @@
 "use client";
-import React from "react"
+import React, { useState } from "react"
 import PhotoAlbum, { Image } from "react-photo-album"
 import NextJsImage from "./NextJsImage"
+import classNames from "classnames";
+import NextImage from "next/image";
 
 type PhotoGalleryProps = {
   images: Image[]
 }
 
 const PhotoGallery: React.FunctionComponent<PhotoGalleryProps> = ({images}: PhotoGalleryProps) => {
-  return <PhotoAlbum
-    layout="rows"
-    photos={images}
-    renderPhoto={NextJsImage}
-    componentsProps={{
-      imageProps: {
-        className: "border border-[1.1rem] border-orange-100 cursor-pointer",
-      },
-    }}
-    breakpoints={[100, 900, 1280]}
-    // defaultContainerWidth={1200}
-    onClick={(photo) => {
-      console.log(photo);
-    }}
-    targetRowHeight={400}
+  const [openPhoto, setOpenPhoto] = useState<Image | null>(null)
+
+  return <div>
+    <div className={classNames("fixed z-10 w-full h-full max-w-screen max-h-screen top-0 left-0", {
+      "hidden": !openPhoto
+    })}>
+      <div
+        className="flex items-center justify-center h-screen opacity-70 bg-black"
+      />
+      {openPhoto && <div
+        className="z-30 absolute inset-0 flex items-center justify-center overflow-hidden"
+        onClick={() => setOpenPhoto(null)}
+      >
+        <div className="relative w-full h-full" style={{ aspectRatio: openPhoto.width / openPhoto.height }}>
+          <NextImage src={openPhoto.src} alt="photo" className={classNames("w-full h-full object-contain")} width={openPhoto.width} height={openPhoto.height} />
+        </div>
+      </div>}
+    </div>
+    <PhotoAlbum
+      layout="rows"
+      photos={images}
+      renderPhoto={NextJsImage}
+      componentsProps={{
+        imageProps: {
+          className: "border border-[1.1rem] border-orange-100 cursor-pointer",
+        },
+      }}
+      breakpoints={[100, 900, 1280]}
+      // defaultContainerWidth={1200}
+      onClick={(photo): void => {
+        console.log("phptp", photo);
+        setOpenPhoto(photo.photo);
+      }}
+      targetRowHeight={400}
     // sizes={{ size: "calc(100vw - 240px)" }}
-  />
+    />
+  </div>
 }
 
 export default PhotoGallery
